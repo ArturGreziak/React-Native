e7714ae9fdb68534369476b02850fdf91cf11aff
Artur Gręziak <alex4448@wp.pl>
2024-01-02 19:13:53 +0100

    StyleSheet

--- components/Greet.js
+++ components/Greet.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Greet({name}) {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Hello {name}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "plum", padding: 40}
+});
