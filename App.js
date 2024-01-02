382e9631a3a556b973154f3a31a91135c88dd090
Artur Gręziak <alex4448@wp.pl>
2024-01-02 18:48:31 +0100

    Component

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { ActivityIndicator, View, Text, Button, Alert  } from "react-native"
+import Greet from './components/Greet';
 
 
 export default function App() { 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "plum", padding: 40}}>
-        <Button title='Alert' onPress={() => Alert.alert("To jest Alert")} />
-        <Button title='Alert2' onPress={() => Alert.alert("To jest Drugi", "Alert 2")} />
-        <Button title='Alert3' onPress={() => Alert.alert("To jest Alert", "Alert3", [
-          {
-            text: "Cancel",
-            onPress: () => alert("Cancel Press")
-          },
-          {
-            text: "Ok",
-            onPress: () => alert("Cancel Press")
-          }
-        ])} />
+        <Greet name="Bruce Wayne" />
+        <Greet name="Clark Kent" />
     </View>
   );
 }
