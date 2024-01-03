eefe22fdcb7919aebdee5f0fe168dcff29426dc0
Artur Gręziak <alex4448@wp.pl>
2024-01-03 01:15:24 +0100

    boxModell

---
+++ components/Box.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet} from "react-native";
+
+export default function Box({childen, style}) {
+    return (
+        <View style={[styles.box, style]}>
+            <Text style={styles.text}>{childen}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({ 
+    box: {
+        backgroundColor: "#ffffff",
+        padding: 20,
+    },
+    text: {
+        fontSize: 34,
+        fontWeight: "bold",
+        textAlign: "center",
+    }
+});
